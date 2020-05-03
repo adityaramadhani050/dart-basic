df63e3884140c0eb576b14c4ceb2ba90db972eb2
ramanozawa <ramanozawa@gmail.com>
2020-05-04 06:26:37 +0700

    operator usage

---
+++ operator.dart
@@ -0,0 +1,70 @@
+/*
+1. Operator Aritmatika
+  Penjumlahan (+)
+  Pengurangan (-)
+  Pembagian (/) yang menghasilkan nilai double.
+  Pembagian (~/) yang menghasilkan nilai integer.
+  Perkalian (*)
+  Sisa bagi atau modulo (%)
+  Increment (++)
+  Derement (--)
+2. Operator Relasi
+  Sama dengan (==)
+  Tidak samadengan (!=)
+  Lebih besar (>)
+  Lebih kecil (<)
+  Lebih besar sama dengan (>=)
+  Lebih kecil sama dengan (<=)
+3. Operator Penugasan
+  Pengisian (=)
+  Penjumlahan (+=)
+  Pengurangan (-=)
+  Pembagian (/=)
+  Perkalian (*=)
+  Sisa Bagi (%=)
+4. Operator Logika
+  And (&&)
+  Or (||)
+  Not (!)
+5. Operator Bitwise
+  And (&)
+  Or (|)
+  Xor (^)
+  Not (~)
+  Left Shift (<<)
+  Right Shift (>>)
+6. Operator Ternary (?)
+*/
+
+import 'dart:io';
+
+main() {
+  print("PROGRAM OPERATOR");
+
+  stdout.write("Nilai a: ");
+  double a = double.parse(stdin.readLineSync());
+  stdout.write("Nilai b: ");
+  double b = double.parse(stdin.readLineSync());
+
+  double hasil;
+
+  // operator penjumlahan
+  hasil = a + b;
+  print("$a + $b = $hasil");
+
+  // operator pengurangan
+  hasil = a - b;
+  print("$a - $b = $hasil");
+
+  // operator perkalian
+  hasil = a * b;
+  print("$a * $b = $hasil");
+
+  // operator pembagian
+  hasil = a / b;
+  print("$a / $b = $hasil");
+
+  // operator sisa bagi
+  hasil = a % b;
+  print("$a % $b = $hasil");
+}
