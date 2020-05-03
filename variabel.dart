9a34a5538cbcd87111dedeed828299a7520300ed
ramanozawa <ramanozawa@gmail.com>
2020-05-04 06:25:47 +0700

    explanation of variable

---
+++ variabel.dart
@@ -0,0 +1,27 @@
+void main() {
+  // membuat variabel dengan kata kunci var
+  // kata kunci var membuat variabel yang dapat diisi dengan tipe data apapun
+  var judul = "Pemrograman Dart";
+  // membuat variabel dengan tipe data
+  // maka variabel tersebut hanya dapat diisi dengan nilai yang sesuai dengan tipe data
+  // 1. tipe data angka : int, double
+  // 2. tipe data teks : String
+  // 3. tipe data boolean : bool
+
+  // tipe data int digunakan untuk bilangan bulat atau desimal
+  int umur = 21;
+  // tipe data double digunakan untuk bilangan pecahan
+  double angkaPecahan = 3.14;
+  // tipe data String digunakan untuk teks
+  String nama = "Adit";
+  // tipe data bool digunakan untuk ekspresi boolean true dan false
+  bool isMarried = false;
+  // membuat variabel menggunakan kata kunci var
+  var alamat = "Rungkut, Surabaya";
+
+  // untuk menampilkan dapat menggunakan perintah print
+  print(judul);
+  print("Nama saya $nama");
+  print("Umur saya $umur tahun");
+  print("Alamat saya $alamat");
+}
